5771ea5c3c1fab6eaddec277fd165442800233c3
Jörg Lauenroth <joerg@lauenroth.org>
2020-09-09 23:27:54 +0200

    time tracking schema

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2020-09-09T21:23:36.630Z
+// 2020-09-09T21:27:32.806Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
