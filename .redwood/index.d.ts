adfd19788b8910cb0471df3f89daaed57985df55
Jörg <joerg@lauenroth.org>
2021-03-17 22:26:06 +0100

    upgrade redwood

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2020-09-11T15:21:28.542Z
+// 2020-09-11T20:01:39.231Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
--- api/src/graphql/timeTrackings.sdl.js
+++ api/src/graphql/timeTrackings.sdl.js
@@ -10,6 +10,7 @@ export const schema = gql`
 
   type Query {
     timeTrackings: [TimeTracking!]!
+    timeTrackingsWeek: [TimeTracking!]!
     timeTracking(id: Int!): TimeTracking
   }
 
