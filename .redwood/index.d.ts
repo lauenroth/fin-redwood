1af1354213cb2aadc9880fcfeaffea94792734b4
Jörg <joerg@lauenroth.org>
2021-04-12 22:16:08 +0200

    upgrade redwood

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2021-03-17T21:23:15.733Z
+// 2021-04-12T20:15:48.727Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
--- .redwood/types/routes.d.ts
+++ .redwood/types/routes.d.ts
@@ -1,25 +1,31 @@
+import '@redwoodjs/router'
+
+type ParamType<constraint> = constraint extends "Int" ? number : constraint extends "Boolean" ? boolean : constraint extends "Float" ? number : string;
+type RouteParams<Route> = Route extends `${string}/{${infer Param}:${infer Constraint}}/${infer Rest}` ? { [Entry in Param]: ParamType<Constraint> } & RouteParams<`/${Rest}`> : Route extends `${string}/{${infer Param}:${infer Constraint}}` ? { [Entry in Param]: ParamType<Constraint> } : Route extends `${string}/{${infer Param}}/${infer Rest}` ? { [Entry in Param]: string } & RouteParams<`/${Rest}`> : {}
+type QueryParams = Record<string | number, string | number | boolean>
+
 declare module '@redwoodjs/router' {
   interface AvailableRoutes {
-    dashboard: () => "/"
-    newTimeTracking: () => "/time-tracking/new"
-    editTimeTracking: () => "/time-tracking/{id:Int}/edit"
-    timeTracking: () => "/time-tracking/{id:Int}"
-    timeTrackings: () => "/time-tracking"
-    invoices: () => "/invoices"
-    editInvoice: () => "/invoices/{id:Int}/edit"
-    invoice: () => "/invoices/{id:Int}"
-    newInvoice: () => "/invoices/new"
-    transactions: () => "/transactions"
-    editTransaction: () => "/transactions/{id:Int}/edit"
-    transaction: () => "/transactions/{id:Int}"
-    newTransaction: () => "/transactions/new"
-    transactionImport: () => "/transaction-import"
-    timeTracking: () => "/time-tracking"
-    newTimeTracking: () => "/time-tracking/new"
-    clients: () => "/clients"
-    editClient: () => "/clients/{id:Int}/edit"
-    client: () => "/clients/{id:Int}"
-    newClient: () => "/clients/new"
-    settings: () => "/settings"
+    dashboard: (params?: RouteParams<"/"> & QueryParams) => "/"
+    newTimeTracking: (params?: RouteParams<"/time-tracking/new"> & QueryParams) => "/time-tracking/new"
+    editTimeTracking: (params?: RouteParams<"/time-tracking/{id:Int}/edit"> & QueryParams) => "/time-tracking/{id:Int}/edit"
+    timeTracking: (params?: RouteParams<"/time-tracking/{id:Int}"> & QueryParams) => "/time-tracking/{id:Int}"
+    timeTrackings: (params?: RouteParams<"/time-tracking"> & QueryParams) => "/time-tracking"
+    invoices: (params?: RouteParams<"/invoices"> & QueryParams) => "/invoices"
+    editInvoice: (params?: RouteParams<"/invoices/{id:Int}/edit"> & QueryParams) => "/invoices/{id:Int}/edit"
+    invoice: (params?: RouteParams<"/invoices/{id:Int}"> & QueryParams) => "/invoices/{id:Int}"
+    newInvoice: (params?: RouteParams<"/invoices/new"> & QueryParams) => "/invoices/new"
+    transactions: (params?: RouteParams<"/transactions"> & QueryParams) => "/transactions"
+    editTransaction: (params?: RouteParams<"/transactions/{id:Int}/edit"> & QueryParams) => "/transactions/{id:Int}/edit"
+    transaction: (params?: RouteParams<"/transactions/{id:Int}"> & QueryParams) => "/transactions/{id:Int}"
+    newTransaction: (params?: RouteParams<"/transactions/new"> & QueryParams) => "/transactions/new"
+    transactionImport: (params?: RouteParams<"/transaction-import"> & QueryParams) => "/transaction-import"
+    timeTracking: (params?: RouteParams<"/time-tracking"> & QueryParams) => "/time-tracking"
+    newTimeTracking: (params?: RouteParams<"/time-tracking/new"> & QueryParams) => "/time-tracking/new"
+    clients: (params?: RouteParams<"/clients"> & QueryParams) => "/clients"
+    editClient: (params?: RouteParams<"/clients/{id:Int}/edit"> & QueryParams) => "/clients/{id:Int}/edit"
+    client: (params?: RouteParams<"/clients/{id:Int}"> & QueryParams) => "/clients/{id:Int}"
+    newClient: (params?: RouteParams<"/clients/new"> & QueryParams) => "/clients/new"
+    settings: (params?: RouteParams<"/settings"> & QueryParams) => "/settings"
   }
 }
