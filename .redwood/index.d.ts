e004fb85e970969b0c9479efdc4cb903c87954e1
Jörg <joerg@lauenroth.org>
2021-05-15 20:33:07 +0200

    upgrade redwood

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2021-05-03T21:29:35.967Z
+// 2021-05-13T19:23:40.445Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
